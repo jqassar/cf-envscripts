cf313b6b650668ecc246eaec6c8bf024340f7b32
Jerry Qassar <jqassar@gmail.com>
2018-03-09 14:23:00 -0800

    Adding initial script to install docker on clean Ubuntu.  Note lack of key verification.

---
+++ preparedockeronubuntu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install docker-ce
+
+sudo cat << EOF > /etc/docker/daemon.json
+{
+  "hosts": ["fd://", "tcp://0.0.0.0:2376"]
+}
+EOF
