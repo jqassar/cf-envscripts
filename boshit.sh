88704e4901b0c251f835f690eb8d7cb3513f1a18
Jerry Qassar <jqassar@gmail.com>
2018-03-09 17:04:18 +0000

    Updated scripts.

--- boshit.sh
+++ boshit.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+
 CERT_DIR="/work"
+WORKSPACE_DIR="/work/bosh-deployment"
 BARG=""
-BCMD="bosh create-env bosh-deployment/bosh.yml"
-BARG="$BARG -o bosh-deployment/docker/cpi.yml"
-BARG="$BARG -o bosh-deployment/jumpbox-user.yml"
+
+BCMD="bosh create-env $WORKSPACE_DIR/bosh.yml"
+
+BARG="$BARG -o $WORKSPACE_DIR/docker/cpi.yml"
+BARG="$BARG -o $WORKSPACE_DIR/jumpbox-user.yml"
 BARG="$BARG --state=state.json"
 BARG="$BARG --vars-store creds.yml"
 BARG="$BARG -v director_name=docker"
 BARG="$BARG -v internal_cidr=10.245.0.0/16"
 BARG="$BARG -v internal_gw=10.245.0.1"
 BARG="$BARG -v internal_ip=10.245.0.10"
-BARG="$BARG -v docker_host=tcp://192.168.99.100:2376"
+BARG="$BARG -v docker_host=tcp://192.168.99.101:2376"
 BARG="$BARG --var-file docker_tls.ca=$CERT_DIR/ca.pem"
 BARG="$BARG --var-file docker_tls.certificate=$CERT_DIR/cert.pem"
 BARG="$BARG --var-file docker_tls.private_key=$CERT_DIR/key.pem"
--- foshit.sh
+++ foshit.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 CFARG=""
+
+WORKSPACE_DIR="/work/cf-deployment"
 SYSTEM_DOMAIN="10.245.0.34.sslip.io"
 
-CFCMD="bosh -e acme -d cf deploy cf-deployment/cf-deployment.yml"
+CFCMD="bosh -e acme -d cf deploy $WORKSPACE_DIR/cf-deployment.yml"
 
 CFARG="$CFARG --vars-store creds.yml"
 CFARG="$CFARG -v system_domain=$SYSTEM_DOMAIN"
-CFARG="$CFARG -o cf-deployment/operations/use-compiled-releases.yml"
-CFARG="$CFARG -o cf-deployment/operations/experimental/use-bosh-dns.yml"
-CFARG="$CFARG -o cf-deployment/operations/experimental/skip-consul-cell-registrations.yml"
-CFARG="$CFARG -o cf-deployment/operations/experimental/skip-consul-locks.yml"
-CFARG="$CFARG -o cf-deployment/operations/experimental/disable-consul.yml"
-CFARG="$CFARG -o cf-deployment/operations/bosh-lite.yml"
-CFARG="$CFARG -o cf-deployment/operations/experimental/disable-consul-bosh-lite.yml"
-CFARG="$CFARG -o cf-deployment/operations/experimental/use-grootfs.yml"
+CFARG="$CFARG -o $WORKSPACE_DIR/operations/scale-to-one-az.yml"
+CFARG="$CFARG -o $WORKSPACE_DIR/operations/use-compiled-releases.yml"
+CFARG="$CFARG -o $WORKSPACE_DIR/operations/experimental/use-bosh-dns.yml"
+CFARG="$CFARG -o $WORKSPACE_DIR/operations/experimental/skip-consul-cell-registrations.yml"
+CFARG="$CFARG -o $WORKSPACE_DIR/operations/experimental/skip-consul-locks.yml"
+CFARG="$CFARG -o $WORKSPACE_DIR/operations/experimental/disable-consul.yml"
+# Only for bosh-lite deployments.
+#CFARG="$CFARG -o $WORKSPACE_DIR/operations/bosh-lite.yml"
+#CFARG="$CFARG -o $WORKSPACE_DIR/operations/experimental/disable-consul-bosh-lite.yml"
+# GrootFS is now on by default.
+#CFARG="$CFARG -o $WORKSPACE_DIR/operations/experimental/use-grootfs.yml"
 
 echo "Running $CFCMD $CFARG..."
 $CFCMD $CFARG
