c9f6910cca8aadc3fd99c56a65d591706ede2d30
Jerry Qassar <jqassar@gmail.com>
2018-03-09 15:00:53 -0800

    Added configurable proxy config.

--- preparedockeronubuntu.sh
+++ preparedockeronubuntu.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# EDIT THIS to match your organization.
+proxyhost="proxy.domain:port"
+export HTTP_PROXY=http://$proxyhost
+export HTTPS_PROXY=https://$proxyhost
+export NO_PROXY="localhost,127.0.0.1"
+
 sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
@@ -16,8 +22,36 @@ sudo apt-get update
 
 sudo apt-get install docker-ce
 
-sudo cat << EOF > /etc/docker/daemon.json
+mkdir ~/.docker
+echo <<- EOF > ~/.docker/config.json
 {
-  "hosts": ["fd://", "tcp://0.0.0.0:2376"]
+ "proxies":
+ {
+   "default":
+   {
+     "httpProxy": "$HTTP_PROXY",
+     "noProxy": "$NO_PROXY"
+   }
+ }
 }
 EOF
+
+# Edit /lib/systemd/system/docker.service
+# TODO: Put in command to append '-H tcp://0.0.0.0:2376' to ExecStart (after '-H fd://')
+
+# Put in proxy settings
+SERVDIR=/etc/systemd/system/docker.service.d
+sudo mkdir -p $SERVDIR
+cat <<- EOF | sudo tee $SERVDIR/http-proxy.conf
+[Service]
+Environment="HTTP_PROXY=$HTTP_PROXY" "NO_PROXY=$NO_PROXY"
+EOF
+
+cat <<- EOF | sudo tee $SERVDIR/https-proxy.conf
+[Service]
+Environment="HTTPS_PROXY=$HTTPS_PROXY" "NO_PROXY=$NO_PROXY"
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo ps aux | grep dockerd
