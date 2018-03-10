265a3f17b9ec7695176c6480aeb8f380b8568068
Jerry Qassar <jqassar@gmail.com>
2018-03-10 23:17:32 +0000

    Removing duplicate bosh-dns entry.

--- foshit.sh
+++ foshit.sh
@@ -11,6 +11,8 @@ CFARG="$CFARG -v system_domain=$SYSTEM_DOMAIN"
 CFARG="$CFARG -o $WORKSPACE_DIR/operations/scale-to-one-az.yml"
 CFARG="$CFARG -o $WORKSPACE_DIR/operations/use-compiled-releases.yml"
 CFARG="$CFARG -o $WORKSPACE_DIR/operations/experimental/use-bosh-dns.yml"
+# #bosh said it was on by default but maybe not in here...?
+CFARG="$CFARG -o $WORKSPACE_DIR/operations/experimental/use-bosh-dns-for-containers.yml"
 CFARG="$CFARG -o $WORKSPACE_DIR/operations/experimental/skip-consul-cell-registrations.yml"
 CFARG="$CFARG -o $WORKSPACE_DIR/operations/experimental/skip-consul-locks.yml"
 CFARG="$CFARG -o $WORKSPACE_DIR/operations/experimental/disable-consul.yml"
@@ -19,10 +21,6 @@ CFARG="$CFARG -o $WORKSPACE_DIR/operations/experimental/disable-consul.yml"
 #CFARG="$CFARG -o $WORKSPACE_DIR/operations/experimental/disable-consul-bosh-lite.yml"
 # GrootFS is now on by default.
 #CFARG="$CFARG -o $WORKSPACE_DIR/operations/experimental/use-grootfs.yml"
-# However, you need bosh-dns for the containers to work.
-# #bosh said it was on by default but maybe not in here...?
-CFARG="$CFARG -o $WORKSPACE_DIR/operations/experimental/use-bosh-dns.yml"
-CFARG="$CFARG -o $WORKSPACE_DIR/operations/experimental/use-bosh-dns-for-containers.yml"
 # Minimize instances/memory.
 CFARG="$CFARG -o $WORKSPACE_DIR/operations/scale-down-instances-and-memory.yml"
 
